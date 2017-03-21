ae3e9ffc66290749f0fffb4c3662ab7c4e5959d5
ramya <ramyashankaran@yahoo.com>
2017-03-20 23:06:16 -0400

    local weather api

---
+++ js/weather.js
@@ -0,0 +1,35 @@
+/* js code for weather app */
+var x = document.getElementById("demo");
+var pos = {
+	latitude:0,
+	longitude:0
+};
+
+function getLocation() {
+    if (navigator.geolocation) {
+ 
+       navigator.geolocation.getCurrentPosition(showPosition);
+      
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+       pos["latitude"] = position.coords.latitude;   
+        pos["longitude"] = position.coords.longitude;
+        
+      x.innerHTML = "Latitude: " + pos["latitude"] + 
+          "<br>Longitude: " + position.coords.longitude;
+      var requestURL = "http://api.openweathermap.org/data/2.5/weather?lat="  +
+                         pos["latitude"] +
+                         "&lon=" +
+                         pos["longitude"]  +     
+                         "&appid=c437da5c2e3ca02811cdc357dd1c6767"; 
+         // AJAX call to get the weather for this location             
+       document.getElementById("urltext").innerHTML = requestURL;
+       $.getJSON(requestURL,null,function(responseData) {
+     document.getElementById("weathertext").innerHTML= "humidity:"+ responseData["main"]["temp"];
+     });
+
+}
